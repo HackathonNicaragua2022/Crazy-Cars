d69523329495eb930d414a88ae17dac6bc9dcff1
Marvin Saravia <marvinsaravia39@gmail.com>
2022-11-12 03:04:25 -0600

    segundo commit

--- app/src/main/java/com/example/kisiapp/pantallajuegosmate1.kt
+++ app/src/main/java/com/example/kisiapp/pantallajuegosmate1.kt
@@ -1,13 +1,21 @@
 package com.example.kisiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class pantallajuegosmate1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantallajuegosmate1)
-    }
 
+
+        val nivel1= findViewById<Button>(R.id.nivel1)
+        nivel1.setOnClickListener {
+            val intent = Intent(this, pantallajuegosmate2::class.java)
+            startActivity(intent)
+        }
+    }
 
 }
--- app/src/main/java/com/example/kisiapp/pantallajuegosmate2.kt
+++ app/src/main/java/com/example/kisiapp/pantallajuegosmate2.kt
@@ -1,11 +1,19 @@
 package com.example.kisiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class pantallajuegosmate2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantallajuegosmate2)
+
+        val volver = findViewById<TextView>(R.id.volver)
+            volver.setOnClickListener {
+            val intent = Intent(this, pantalla2::class.java)
+            startActivity(intent)
+        }
     }
 }
